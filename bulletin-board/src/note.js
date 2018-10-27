95d887acdc60f05d7f5b27ec46c2a463225990ce
GW <garviswbs@gmail.com>
2018-10-26 20:49:28 -0600

    finished app

--- bulletin-board/src/note.js
+++ bulletin-board/src/note.js
@@ -4,6 +4,7 @@ import { FaPencilAlt } from 'react-icons/fa'
 import { FaSave } from 'react-icons/fa'
 
 class Note extends Component {
+
     constructor(props) {
         super(props)
         this.state = {
@@ -14,18 +15,45 @@ class Note extends Component {
         this.save = this.save.bind(this)
         this.renderForm = this.renderForm.bind(this)
         this.renderDisplay = this.renderDisplay.bind(this)
+        this.randomBetween = this.randomBetween.bind(this)
+    }
+
+    componentWillMount() {
+        this.style = {
+            right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+            top: this.randomBetween(0, window.innerHeight - 150, 'px'),
+            transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
+
+        }
+    }
+
+    randomBetween(x, y, s) {
+        return x + Math.ceil(Math.random() * (y - x)) + s
+    }
+
+    componentDidUpdate() {
+        var textArea
+        if (this.state.editing) {
+            textArea = this._newText
+            textArea.focus()
+            textArea.select()
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (
+            this.props.children !== nextProps.children || this.state !== nextState
+        )
     }
 
     edit() {
         this.setState({
             editing: true
         })
-        console.log("Edit State", this.setState)
-
     }
 
     remove() {
-        alert("REMOVING NOTE")
+        this.props.onRemove(this.props.index)
     }
 
     save(e) {
@@ -34,14 +62,14 @@ class Note extends Component {
         this.setState({
             editing: false
         })
-        console.log("SAVE State", this.setState)
     }
 
     renderForm() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <form onSubmit={this.save}>
-                    <textarea ref={input => this._newText = input} />
+                    <textarea ref={input => this._newText = input}
+                        defaultValue={this.props.children} />
                     <button id="save"><FaSave /></button>
                 </form>
             </div>
@@ -50,7 +78,7 @@ class Note extends Component {
 
     renderDisplay() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit} id="edit"><FaPencilAlt /></button>
